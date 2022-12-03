722fa2c2d6e2de9a2e3db4ebf1e003bcfe2572fc
Nicholas Chin <nic.c3.14@gmail.com>
2022-12-02 22:47:42 -0700

    Day 3: Improve python solution
    Use sets for day 1 and remove workaround in part 2 that turned out to be
    due to an extra newline I accidentally added to the input

--- day3/part1.py
+++ day3/part1.py
@@ -1,17 +1,7 @@
 import sys
 total = 0
 for line in sys.stdin:
-    priorities = [ord(c)-96 if c > 'Z' else ord(c)-64+26 for c in line.strip()]
-    a = sorted(priorities[0:len(priorities)//2])
-    b = sorted(priorities[len(priorities)//2:])
-    bIndex = 0
-    aIndex = 0
-    while max(aIndex, bIndex) < len(a):
-        if a[aIndex] == b[bIndex]:
-            total += a[aIndex]
-            break
-        if a[aIndex] > b[bIndex]:
-            bIndex += 1
-        else:
-            aIndex += 1
+    a, b = [set(line.strip()[0:len(line.strip())//2]), set(line.strip()[len(line.strip())//2:])]
+    c = a.intersection(b).pop()
+    total += ord(c)-96 if c > 'Z' else ord(c)-64+26
 print(total)
